28105abb5c0581a3f050f134ef8ff8a3a864f070
Chewei Hu <chewei05@users.noreply.github.com>
2016-08-29 16:07:04 +0800

    New route and controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [ 'as' => 'install.index', 'uses' => 'InstallController@index', function () {
+}]);
+Route::get('/home', [ 'as' => 'home.index', 'uses' => 'HomeController@index', function () {
+}]);
+Route::get('/post/{id}', [ 'as' => 'home.post', 'uses' => 'HomeController@show', function () {
+}]);
+
+Route::get('/signin', [ 'as' => 'signin.index', 'uses' => 'SigninController@index', function () {
+}]);
+Route::get('/signout', [ 'as' => 'signout.index', 'uses' => 'SignoutController@index', function () {
+}]);
+
+Route::get('/admin', [ 'as' => 'admin.index', 'uses' => 'AdminController@index', function () {
+}]);
+Route::get('/admin/post', [ 'as' => 'post.index', 'uses' => 'PostController@index', function () {
+}]);
+Route::get('/admin/post/show/{id}', [ 'as' => 'post.show', 'uses' => 'PostController@show', function () {
+}]);
+Route::get('/admin/post/modify/{id}', [ 'as' => 'post.modify', 'uses' => 'PostController@edit', function () {
+}]);
+Route::get('/admin/post/new', [ 'as' => 'post.new', 'uses' => 'PostController@create', function () {
+}]);
